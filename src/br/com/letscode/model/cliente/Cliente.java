09c88d2c8a0fde34b273d60da2755a7f5257eefb
brunocecilio <brunofc@live.com>
2022-07-08 16:06:29 -0300

    fix: initializes contas array format Cliente class

--- src/br/com/letscode/model/cliente/Cliente.java
+++ src/br/com/letscode/model/cliente/Cliente.java
@@ -4,47 +4,52 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import br.com.letscode.model.conta.Conta;
-import br.com.letscode.model.conta.TipoContaEnum;   
+import br.com.letscode.model.conta.TipoContaEnum;
 
 public abstract class Cliente {
     private String id;
     private static int idSequence = 1;
     private String nome;
-    private ArrayList<Conta> contas;
+    private ArrayList<Conta> contas = new ArrayList<Conta>();
 
-    public String getId(){
+    public String getId() {
         return id;
     }
-    public void setId(int id){
+
+    public void setId(int id) {
         this.id = String.valueOf(id);
     }
-    public static int nextId(){
+
+    public static int nextId() {
         return idSequence++;
     }
+
     public String getNome() {
         return nome;
-    } 
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
     public Conta getConta(int codigoConta) {
         return contas.stream().filter(conta -> conta.getCodigoConta() == codigoConta)
-            .findFirst().orElse(null);
+                .findFirst().orElse(null);
     }
+
     public ArrayList<Conta> getContas() {
         return contas;
     }
 
-    protected void addConta(Conta conta){
+    protected void addConta(Conta conta) {
         this.contas.add(conta);
     }
 
-    public BigDecimal getRendimentoContaPoupanca(){
+    public BigDecimal getRendimentoContaPoupanca() {
         return new BigDecimal(1);
     }
 
-    public BigDecimal getRendimentoContaInvestimento(){
+    public BigDecimal getRendimentoContaInvestimento() {
         return new BigDecimal(1);
     }
 
