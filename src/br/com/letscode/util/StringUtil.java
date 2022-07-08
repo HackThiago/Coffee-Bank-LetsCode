e46dae0c3aa76c9bd3081d04802a01ce9db81ccf
brunocecilio <brunofc@live.com>
2022-07-08 16:10:10 -0300

    feat: new functions in StringUtil new functions list: addArgToList; removeArgFromList; formatCurrencyBRL; formatCPF; formatCNPJ;
    refactors blankSpaces function

--- src/br/com/letscode/util/StringUtil.java
+++ src/br/com/letscode/util/StringUtil.java
@@ -1,6 +1,12 @@
 package br.com.letscode.util;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class StringUtil {
+    private static final NumberFormat CURRENCY_FORMATTER = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+
     public static String multiply(String str, int times) {
         String newStr = "";
         for (int i = 0; i < times; i++) {
@@ -10,11 +16,7 @@ public class StringUtil {
     }
 
     public static String blankSpaces(int quantity) {
-        String spaces = "";
-        for (int i = 0; i < quantity; i++) {
-            spaces = spaces.concat(" ");
-        }
-        return spaces;
+        return multiply(" ", quantity);
     }
 
     public static String addBlankSpacesToAllLines(String s, int quantity) {
@@ -38,6 +40,58 @@ public class StringUtil {
         }
         int spacesQuantity = (lineWidth - maxLineLength) / 2;
         s = addBlankSpacesToAllLines(s, spacesQuantity);
+
         return s;
+    }
+
+    public static String[] addArgToList(String[] args, String newArg) {
+        String[] newArgs = new String[args.length + 1];
+
+        for (int i = 0; i < args.length; i++) {
+            newArgs[i] = args[i];
+        }
+
+        newArgs[args.length] = newArg;
+
+        return newArgs;
+    }
+
+    public static String[] removeArgFromList(String[] args, int index) {
+        String[] newArgs = new String[args.length - 1];
+
+        int pos = 0;
+        for (int i = 0; i < args.length; i++) {
+            if (i != index) {
+                newArgs[pos++] = args[i];
+            }
+        }
+
+        return newArgs;
+    }
+
+    public static String formatCurrencyBRL(BigDecimal amount) {
+        return CURRENCY_FORMATTER.format(amount);
+    }
+
+    public static String formatCPF(String cpf) {
+        return cpf.substring(0, 3)
+                + "."
+                + cpf.substring(3, 6)
+                + "."
+                + cpf.substring(6, 9)
+                + "-"
+                + cpf.substring(9);
+    }
+
+    public static String formatCNPJ(String cnpj) {
+        return cnpj.substring(0, 2)
+                + "."
+                + cnpj.substring(2, 5)
+                + "."
+                + cnpj.substring(5, 8)
+                + "/"
+                + cnpj.substring(8, 12)
+                + "-"
+                + cnpj.substring(12);
     }
 }
