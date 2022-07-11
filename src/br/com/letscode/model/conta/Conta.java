a573f34c77096b49586821660e3a2b8e454b5207
brunocecilio <brunofc@live.com>
2022-07-11 15:01:16 -0300

    fix: change errors to exceptions fix calculations of the methods
    Co-authored-by: HackThiago <thr8_2006@yahoo.com.br>

--- src/br/com/letscode/model/conta/Conta.java
+++ src/br/com/letscode/model/conta/Conta.java
@@ -3,8 +3,9 @@ package br.com.letscode.model.conta;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import br.com.letscode.error.InvalidCommandException;
 import br.com.letscode.model.cliente.Cliente;
-import br.com.letscode.model.cliente.ClientePF;
+import br.com.letscode.model.cliente.ClientePJ;
 
 public abstract class Conta {
     protected static int qtdContas = 0;
@@ -28,37 +29,41 @@ public abstract class Conta {
         return this.saldo;
     }
 
-    private void cobrarTaxaOperacao() {
-        // cobra 0.5% por cada saque e transf para PFs
-        if (cliente instanceof ClientePF) {
-            saldo = saldo.multiply(BigDecimal.valueOf(0.995));
+    private void cobrarTaxaOperacao(BigDecimal value) {
+        // cobra 0.5% por cada saque e transf para PJs
+        if (cliente instanceof ClientePJ) {
+            this.saldo = saldo.subtract(BigDecimal.valueOf(0.005).multiply(value));
         }
     }
 
-    public void sacar(BigDecimal quantia) {
+    public void sacar(BigDecimal quantia) throws InvalidCommandException {
+        if (quantia.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidCommandException("Quantia inválida!");
+        }
         if (saldo.compareTo(quantia) >= 0) {
             saldo = saldo.subtract(quantia);
         } else {
-            throw new Error("Saldo Insuficiente");
+            throw new InvalidCommandException("Saldo insuficiente!");
         }
 
-        this.cobrarTaxaOperacao();
+        this.cobrarTaxaOperacao(quantia);
     }
 
-    public void depositar(BigDecimal quantia) {
-        this.saldo = this.saldo.add(quantia);
-    };
+    public abstract void depositar(BigDecimal quantia) throws InvalidCommandException;
 
-    public void transferir(Conta destinatario, BigDecimal quantia) {
+    public void transferir(Conta destinatario, BigDecimal quantia) throws InvalidCommandException {
         final int LESS_THAN = -1;
+        if (quantia.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidCommandException("Quantia inválida!");
+        }
         if (this.consultarSaldo().compareTo(quantia) == LESS_THAN) {
-            throw new Error("Saldo insuficiente");
+            throw new InvalidCommandException("Saldo insuficiente!");
         }
 
         saldo = saldo.subtract(quantia);
         destinatario.saldo = destinatario.saldo.add(quantia);
 
-        this.cobrarTaxaOperacao();
+        this.cobrarTaxaOperacao(quantia);
     };
 
     public void efetuarRendimento() {
