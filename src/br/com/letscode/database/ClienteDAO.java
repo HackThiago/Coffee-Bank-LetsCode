4dd4cd2e0c2b6ed22a1255fe171224aaa2c7df87
Bruno Faustino Cecilio <49068550+brunocecilio@users.noreply.github.com>
2022-07-11 01:21:03 -0300

    minor: delete old mock database methods

--- src/br/com/letscode/database/ClienteDAO.java
+++ src/br/com/letscode/database/ClienteDAO.java
@@ -1,14 +1,12 @@
 package br.com.letscode.database;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import br.com.letscode.error.ClientNotFoundException;
 import br.com.letscode.model.cliente.Cliente;
 import br.com.letscode.model.cliente.ClientePF;
 import br.com.letscode.model.cliente.ClientePJ;
-import br.com.letscode.model.conta.TipoContaEnum;
 import br.com.letscode.util.ArrayListUtil;
 
 public class ClienteDAO {
@@ -96,40 +94,4 @@ public class ClienteDAO {
         }
     }
 
-    public static List<Cliente> getMockClientList() {
-        List<Cliente> clients = new ArrayList<Cliente>();
-
-        ClientePF cliente1 = new ClientePF();
-        cliente1.setNome("Teste Cliente PF");
-        cliente1.setCpf("11111111111");
-
-        ClientePJ cliente2 = new ClientePJ();
-        cliente2.setNome("Teste Cliente PJ");
-        cliente2.setCnpj("22222222222222");
-
-        clients.add(cliente1);
-        clients.add(cliente2);
-
-        return clients;
-    }
-
-    public static Cliente getMockClient() {
-        ClientePF cliente = new ClientePF();
-        cliente.setNome("Teste Cliente PF");
-        cliente.setCpf("11111111111");
-
-        cliente.abrirConta(TipoContaEnum.CORRENTE);
-
-        return cliente;
-    }
-
-    public static Cliente getMockClient2() {
-        ClientePJ cliente = new ClientePJ();
-        cliente.setNome("Teste Cliente PJ");
-        cliente.setCnpj("22222222222222");
-
-        cliente.abrirConta(TipoContaEnum.INVESTIMENTO);
-
-        return cliente;
-    }
 }
